add0b69b3d4a393e6333d29455299848eafcbcfb
raselcsedev <raselcse.dev@gmail.com>
2024-05-21 22:14:47 +0600

    instructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 // import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
-// import Instructors from "@/components/Instructors";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -16,7 +16,7 @@ export default function Home() {
       <WhyChooseUs />
       <MusicSchoolTestimonials />
       <UpcomingWebinars />
-      {/* <Instructors /> */}
+      <Instructors />
       {/* <Footer />  */}
     </main>
   );
