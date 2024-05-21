13862c806703d957470bd4263f18886724e38d8d
raselcsedev <raselcse.dev@gmail.com>
2024-05-21 20:21:55 +0600

    hero section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 // import FeaturedCourses from "@/components/FeaturedCourses";
 // import Footer from "@/components/Footer";
-// import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/HeroSection";
 // import Instructors from "@/components/Instructors";
 // import MusicSchoolTestimonials from "@/components/TestimonialCards";
 // import UpcomingWebinars from "@/components/UpcomingWebinars";
@@ -11,14 +11,13 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      hiiiiiii
-      {/* <HeroSection />
-      <FeaturedCourses />
-      <WhyChooseUs />
-      <MusicSchoolTestimonials />
-      <UpcomingWebinars />
-      <Instructors />
-      <Footer /> */}
+      <HeroSection />
+      {/* <FeaturedCourses /> */}
+      {/* <WhyChooseUs /> */}
+      {/* <MusicSchoolTestimonials /> */}
+      {/* <UpcomingWebinars /> */}
+      {/* <Instructors /> */}
+      {/* <Footer />  */}
     </main>
   );
 }
